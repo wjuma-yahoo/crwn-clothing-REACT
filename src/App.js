b01b7252874777fdbbfe296c9de2d50b2565fb06
wjuma-yahoo <wjuma19dev@yahoo.com>
2022-08-15 09:36:02 -0400

    Creado componente category-item

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
+import './App.scss'
+import { CategoryItem } from './components/category-item/category-item.component'
+
 const App = () => {
 
 
   const catergories = [
     {
       id: 1,
-      title: 'Hacks'
+      title: 'Hacks',
+      imageUrl: 'https://m.media-amazon.com/images/I/81a48YfJyPL._AC_UL320_.jpg'
     },
     {
       id: 2,
-      title: 'Jackets'
+      title: 'Jackets',
+      imageUrl: 'https://m.media-amazon.com/images/I/51okOH2sQcL._AC_UL320_.jpg'
     },
     {
       id: 3,
-      title: 'Sneakers'
+      title: 'Sneakers',
+      imageUrl: 'https://m.media-amazon.com/images/I/71-8yqdFzNL._AC_UL320_.jpg'
     },
     {
       id: 4,
-      title: 'Womens'
+      title: 'Womens',
+      imageUrl: 'https://m.media-amazon.com/images/I/919Hht2azRL._AC_UL320_.jpg'
     },
     {
       id: 5,
-      title: 'Mens'
+      title: 'Mens',
+      imageUrl: 'https://m.media-amazon.com/images/I/51qSgaDeJOL._AC_UL320_.jpg'
     }
   ]
 
   return (
     <div className='categories-container'>
       {
-        catergories.map(({title}) => (
-          <div className='category-container'>
-
-            <div className="background-image" />
-
-            <div className='category-body-container'>
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-
-          </div>
+        catergories.map((category) => (
+         <CategoryItem key={category.id} category={category} />
         ))
       }
     </div>
